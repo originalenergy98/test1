2364bdd3ea1dd017e026ea39cbb01d4792f87a99
Александра <igoshina.sashenka98@gmail.com>
2022-02-14 15:51:33 +0700

    add program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿string[] array = {"654", "hello", "*&K^$#", "@", "7", "dex"};
+    for (int i = 0; i < array.Length; i++){
+        if (array[i].Length < 4){
+            Console.WriteLine(array[i]);
+        }
+    }
